bb8e4a852e83f3a78c02574d993a6a0dbe055b2d
tianjiabao <t2847202298@163.com>
2024-11-29 03:32:38 +0800

    小调整，添加配置文件

--- main_source.py
+++ main_source.py
@@ -5,15 +5,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-# 配置信息
-phone = "15986373998"
-pwd = "Asdfghjkl456"
-url1 = "https://onlineweb.zhihuishu.com/"
-url2 = "https://hike.zhihuishu.com/aidedteaching/sourceLearning/sourceLearning?courseId=10914055&fileId=22409575"
-
 # 初始化浏览器
 def init_browser():
     return webdriver.Chrome()
+
+# 读取配置信息
+def read_config(file_path):
+    config = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                key, value = line.split('=', 1)
+                config[key.strip()] = value.strip().strip('"')
+    return config
 
 # 登录
 def login(browser, phone, pwd, url1):
@@ -35,37 +40,31 @@ def wait_for_element(browser, by, value, timeout=60):
             pass
     raise TimeoutException("等待页面元素超时")
 
-# 格式化时间
-def format_time(seconds):
-    if seconds is None:
-        return "00:00"
-    minutes = int(seconds // 60)
-    seconds = int(seconds % 60)
-    return f"{minutes:02}:{seconds:02}"
 
 # 查找未完成视频
 def find_unfinished_videos(browser):
     icon_video_elements = browser.find_elements(By.CSS_SELECTOR, 'i.icon-video')
     icon_finish_elements = browser.find_elements(By.CSS_SELECTOR, 'i.icon-finish')
-    print(f"找到的视频图标数量: {len(icon_video_elements)}")
-    print(f"找到的完成图标数量: {len(icon_finish_elements)}")
+    print(f"网页存在的视频总数: {len(icon_video_elements)}")
+    print(f"已观看的视频数量: {len(icon_finish_elements)}")
 
     icon_video_parents = [video.find_element(By.XPATH, './..') for video in icon_video_elements]
     icon_finish_parents = [finish.find_element(By.XPATH, './../..') for finish in icon_finish_elements]
 
-    print(f"未完成视频图标父元素数量: {len(icon_video_parents)}")
-    print(f"已完成视频图标父元素数量: {len(icon_finish_parents)}")
-
     icon_unfinish_parents = [parent for parent in icon_video_parents if parent not in icon_finish_parents]
-    print(f"筛选出的未完成视频数量: {len(icon_unfinish_parents)}")
+    print(f"未观看完成的视频数量: {len(icon_unfinish_parents)}")
 
     return icon_unfinish_parents
 
 # 播放视频并等待结束
-def play_video_and_wait(browser, video_player, parent_name):
+def play_video_and_wait(browser, video_player, parent_name, mute_video):
     video_player = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.TAG_NAME, "video"))
     )
+
+    if mute_video:
+        browser.execute_script("arguments[0].muted = true;", video_player)
+
     browser.execute_script("arguments[0].play();", video_player)
     print(f"视频：{parent_name} |开始播放")
 
@@ -86,8 +85,24 @@ def play_video_and_wait(browser, video_player, parent_name):
     except TimeoutException:
         print("视频播放超时。")
 
+def check_config(phone, pwd, url1, url2):
+    if not phone or not pwd or not url1 or not url2:
+        print("配置文件不完整，请检查！")
+
 # 主函数
 def main():
+    config = read_config('config.txt')
+    phone = config.get('phone')
+    pwd = config.get('pwd')
+    url1 = config.get('url1')
+    url2 = config.get('url2')
+    mute_video = config.get('muteVideo', 'false').lower() == 'true'  # 默认静音
+
+
+    if not phone or not pwd or not url1 or not url2:
+        print("配置文件不完整，请检查！")
+        return  # 退出程序
+
     browser = init_browser()
     try:
         login(browser, phone, pwd, url1)
@@ -108,7 +123,7 @@ def main():
             parent_name = parent.find_element(By.CLASS_NAME, "file-name").text
             parent.click()
 
-            play_video_and_wait(browser, video_player, parent_name)
+            play_video_and_wait(browser, video_player, parent_name, mute_video)
 
     except Exception as e:
         print(f"错误: {e}")
